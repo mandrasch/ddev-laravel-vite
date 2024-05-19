dcb244dce8b10025394717362806c22e0576441e
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-05-19 10:08:56 +0200

    refactor: clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,12 @@ export default defineConfig({
         }),
     ],
     server: {
-        // respond to all network requests (same as '0.0.0.0')
-        host: true,
-        // we need a strict port to match on PHP side
+        host: true, // respond to all network requests
         strictPort: true,
         port: 5173,
         hmr: {
-            // TODO: Is this the best way to achieve that? 🤔
-            // Force the Vite client to connect via SSL
-            // This will also force a "https://" URL in the public/hot file
             protocol: 'wss',
-            // The host where the Vite dev server can be accessed
-            // This will also force this host to be written to the public/hot file
             host: `${process.env.DDEV_HOSTNAME}`,
         }
     },
-
 });
